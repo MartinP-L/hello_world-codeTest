33ed34ca7e9d7ebb5e2eb247a80c3ff71f85a4a3
MartinP-L <martin.puyou@gmail.com>
2019-04-15 00:38:09 +0200

    ce sont des tests

---
+++ test github/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello world!" << std::endl;
+
+    std::cout << "il est l'heure du premier test" << std::endl;
+    std::cout << "il est l'heure du premier test" << std::endl;
+    std::cout << "il est l'heure du premier test" << std::endl;
+    hein ?
+
+
+    return 0;
+}
